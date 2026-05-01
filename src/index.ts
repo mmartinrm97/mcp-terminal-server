81d0f717377db3593d4437cd281631e7746ba464
Miguel Ramos <mmartinrm3@gmail.com>
2026-05-01 05:09:45 -0500

    chore: bump version to 0.1.4

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ import { SessionManager } from "./core/session-manager.js";
 import { createTerminalServer } from "./server.js";
 import type { SessionManagerConfig } from "./types.js";
 
-const PKG_VERSION = "0.1.3";
+const PKG_VERSION = "0.1.4";
 
 // ---------------------------------------------------------------------------
 // Configuration via environment variables
